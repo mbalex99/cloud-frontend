cdd9fe8a2e8d8c34cec4616730c86bee47f1db20
Maximilian Alexander <mbalex99@gmail.com>
2017-12-21 06:46:13 -0800

    fixing front list

--- src/scenes/projects/scenes/list/ListScene.tsx
+++ src/scenes/projects/scenes/list/ListScene.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
-import { List, Layout, Row, Col, Icon, Avatar, Input } from 'antd'
+import { List, Layout, Row, Col, Icon, Avatar, Input, Button } from 'antd'
 import * as Recharts from 'recharts'
 import * as faker from 'faker'
 import { LineChart, Line } from 'recharts';
 
 const Content = Layout.Content
 const Search = Input.Search
+const ButtonGroup = Button.Group;
 
 interface Project {
     projectId: string
@@ -14,9 +15,17 @@ interface Project {
     accountType: 'Dev 1' | 'Dev 2' | 'Dev 3' | 'Pro 1' | 'Pro 2'
 }
 
+interface Doc {
+    title: string
+    summary: string
+}
+
 interface ListState {
     projects: Project[]
+    docs: Doc[]
 }
+
+const logoSVG = require('../../../../assets/img/logo.svg')
 
 export class ListScene extends React.Component<{}, ListState> {
 
@@ -56,8 +65,18 @@ export class ListScene extends React.Component<{}, ListState> {
             }
         ]
 
+        let docs: Doc[] = []
+
+        for (let index = 0; index < 5; index++) {
+            docs.push({
+                title: faker.random.words(3),
+                summary: faker.lorem.sentence(2)
+            })
+        }
+
         this.state = {
-            projects: projects
+            projects: projects,
+            docs: docs
         }
     }
 
@@ -82,8 +101,8 @@ export class ListScene extends React.Component<{}, ListState> {
         return (
             <Row>
                 <Col span={8}>
-                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
-                        <h1>Welcome to Realm Cloud.</h1>
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 600 }}>
+                        <h1><img src={logoSVG} height={28} width={28} /> Welcome to Realm Cloud.</h1>
                         <span>
                             Build <strong>powerful</strong>, engaging real-time apps that work even when there's no internet.
                         </span>
@@ -92,7 +111,7 @@ export class ListScene extends React.Component<{}, ListState> {
                             <a><Icon type="file-text" />View the Docs</a>
                         </span>
                         <Search
-                            style={ { marginTop: 16, marginBottom: 16 } }
+                            style={{ marginTop: 16, marginBottom: 16 }}
                             placeholder="Search for Your Project"
                             onSearch={value => console.log(value)}
                             enterButton
@@ -101,13 +120,73 @@ export class ListScene extends React.Component<{}, ListState> {
                             itemLayout="horizontal"
                             dataSource={this.state.projects}
                             renderItem={(item: Project) => (
+                                <a href={`/projects/${item.projectId}/data`}>
                                 <List.Item>
                                     <List.Item.Meta
-                                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                        avatar={<Avatar src={logoSVG} />}
                                         title={<a href={`/projects/${item.projectId}/data`}> {item.title}</a>}
                                         description={item.notes}
                                     />
                                     {this.generateChart()}
+                                </List.Item>
+                                </a>
+                            )}
+                        />
+                    </Content>
+                </Col>
+                <Col span={8}>
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 600 }}>
+                        <h1>Create a New Project</h1>
+                        <span>
+                            Get started with a project quickly with our boilerplate projects.
+                        </span>
+                        <Icon type="plus" style={{ width: '100%', marginTop: 20, textAlign: 'center', fontSize: '50px' }} />
+                        <ButtonGroup style={{ width: '100%', marginTop: 80 }}>
+                            <Button type="primary" size="large" style={{ width: '33%' }}>
+                                <Icon type="apple" />iOS
+                            </Button>
+                            <Button type="primary" size="large" style={{ width: '33%' }}>
+                                <Icon type="android" />Android
+                            </Button>
+                            <Button type="primary" size="large" style={{ width: '33%' }}>
+                                <Icon type="code" />Node
+                            </Button>
+                        </ButtonGroup>
+                        <h2 style={{ width: '100%', marginTop: 80 }}>Or get started with Chat</h2>
+                        <ButtonGroup style={{ width: '100%', marginTop: 20 }}>
+                            <Button size="large" style={{ width: '33%' }}>
+                                <Icon type="apple" />iOS
+                            </Button>
+                            <Button size="large" style={{ width: '33%' }}>
+                                <Icon type="android" />Android
+                            </Button>
+                            <Button size="large" style={{ width: '33%' }}>
+                                <Icon type="code" />Node
+                            </Button>
+                        </ButtonGroup>
+                    </Content>
+                </Col>
+                <Col span={8}>
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 600 }}>
+                        <h1>Search our Docs</h1>
+                        <span>
+                            Find the resources quickly 
+                        </span>
+                        <Search
+                            style={{ marginTop: 16, marginBottom: 16 }}
+                            placeholder="Search for Your Project"
+                            onSearch={value => console.log(value)}
+                            enterButton
+                        />
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={this.state.docs}
+                            renderItem={(doc: Doc) => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        title={<a>{doc.title}</a>}
+                                        description={doc.summary}
+                                    />
                                 </List.Item>
                             )}
                         />
