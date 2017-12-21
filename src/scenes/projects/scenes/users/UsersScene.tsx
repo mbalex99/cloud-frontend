d84af8810934af44311e5aa51553e23762fac67c
Maximilian Alexander <mbalex99@gmail.com>
2017-12-21 06:52:49 -0800

    adding search bar to users

--- src/scenes/projects/scenes/users/UsersScene.tsx
+++ src/scenes/projects/scenes/users/UsersScene.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 import * as faker from 'faker'
 import * as _ from 'lodash'
 import * as moment from 'moment'
-import { List, Layout, Row, Col, Icon, Menu, Table, Checkbox } from 'antd'
+import { List, Layout, Row, Col, Icon, Menu, Table, Checkbox, Input } from 'antd'
 import { Button } from 'antd/lib/radio';
 
 const Sider = Layout.Sider
 const Content = Layout.Content
-
+const Search = Input.Search
 
 interface User {
     userId: string
@@ -81,6 +81,12 @@ export class UsersScene extends React.Component<{}, UsersSceneState> {
                 <Col span={24}>
                     <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280, height: '100%' }}>
                         <h1><Icon type="user" /> Users</h1>
+                        <Search
+                            style={{ marginTop: 16, marginBottom: 16 }}
+                            placeholder="Filter data"
+                            onSearch={value => console.log(value)}
+                            enterButton
+                        />
                         <Table pagination={{ pageSize: 25 }} size="small" rowKey="userId" dataSource={this.state.users} columns={COLUMNS} />
                     </Content>
                 </Col>
